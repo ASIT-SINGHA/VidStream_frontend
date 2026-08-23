be964caef346f6e70a71a371306e0f5a6886188f
asit singha <asitkumarsingha34@gmail.com>
2026-08-24 01:26:21 +0530

    implement routing with public and protected layouts; add Home, Login, and Register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
+import { Routes, Route } from 'react-router';
+import { Container, PublicLayout, ProtectedLayout } from './components/index.js';
+import { Register, Login, Home } from './pages/PageExports.js';
 
+function App() {
   return (
-   <div>
-    <h1>start</h1>
-   </div>
-  )
+    <Container>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        {/*private routes */}
+        <Route element={<ProtectedLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
