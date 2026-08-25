e6ed134570f060dd04c3a22e2896e2db452a80cb
asit singha <asitkumarsingha34@gmail.com>
2026-08-25 20:11:16 +0530

    add Input component and update index.js to export it

---
+++ src/components/Ui/Input.jsx
@@ -0,0 +1,35 @@
+import { Container } from '../index.js';
+import { useId } from 'react';
+
+export default function Input({
+  register,
+  type = 'text',
+  placeholder,
+  accept,
+  className = '',
+  label,
+  error = '',
+  ref,
+  register,
+  ...props
+}) {
+  const id = useId();
+  let ifFile = false;
+  if (type == 'file') ifFile = true;
+  return (
+    <Container>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input
+        type={type}
+        id={id}
+        {...register}
+        accept={accept || ifFile ? 'image/*' : undefined}
+        className={className}
+        placeholder={placeholder}
+        {...props}
+        ref={ref}
+      />
+      {error && <FormError message={error.message ?? error} />}
+    </Container>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-import ProtectedLayout from './Layout/ProtectedLayout';
-import PublicLayout from './Layout/PublicLayout';
-import Container from './Ui/Container';
-export { ProtectedLayout, PublicLayout, Container };
+import ProtectedLayout from './Layout/ProtectedLayout.jsx';
+import PublicLayout from './Layout/PublicLayout.jsx';
+import Container from './Ui/Container.jsx';
+import Btn from './Ui/Btn.jsx';
+import Input from './Ui/Input.jsx';
+export { ProtectedLayout, PublicLayout, Container, Btn, Input };
