78751bb75c952d5a578a554518faf4202c8dd220
asit singha <asitkumarsingha34@gmail.com>
2026-08-26 01:13:39 +0530

    refactor user service functions to use async/await for improved readability and error handling

--- src/services/user.js
+++ src/services/user.js
@@ -1,30 +1,32 @@
 import axiosInstead from './api';
 
-export const registerUser = (formData) =>
-  axiosInstead.past('/users/register', formData, {
+export const registerUser = async (formData) =>
+  await axiosInstead.past('/users/register', formData, {
     Headers: {
       'Content-Type': 'multipart/form-data',
     },
   });
-export const login = (data) => axiosInstead.post('/users/login', data);
-export const logout = (data) => axiosInstead.post('/users/logout', data);
-export const refreshToken = (data) => axiosInstead.post('/users/refresh-token', data);
-export const changePassword = (data) => axiosInstead.post('/users/change-password', data);
-export const getCurrentUser = (data) => axiosInstead.get('/users/get-user', data);
-export const updateAccoundDetails = (data) =>
-  axiosInstead.patch('/users/update-accound-details', data);
-export const updateAvatar = (formData) =>
-  axiosInstead.patch('/users/update-avatar', formData, {
+export const login = async (data) => await axiosInstead.post('/users/login', data);
+export const logout = async (data) => await axiosInstead.post('/users/logout', data);
+export const refreshToken = async (data) => await axiosInstead.post('/users/refresh-token', data);
+export const changePassword = async (data) =>
+  await axiosInstead.post('/users/change-password', data);
+export const getCurrentUser = async (data) => await axiosInstead.get('/users/get-user', data);
+export const updateAccoundDetails = async (data) =>
+  await axiosInstead.patch('/users/update-accound-details', data);
+export const updateAvatar = async (formData) =>
+  await axiosInstead.patch('/users/update-avatar', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
   });
-export const updateCoverImage = (formData) =>
-  axiosInstead.patch('/users/update-coverImage', formData, {
+export const updateCoverImage = async (formData) =>
+  await axiosInstead.patch('/users/update-coverImage', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
   });
-export const getUserChannel = (data) => axiosInstead.get('/users/c/:username', data);
-export const getUserWatchHistory = (data) => axiosInstead.get('/users/watch-history', data);
-export const createChannel = (data) => axiosInstead.post('/users/create-channel', data);
+export const getUserChannel = async (data) => await axiosInstead.get('/users/c/:username', data);
+export const getUserWatchHistory = async (data) =>
+  await axiosInstead.get('/users/watch-history', data);
+export const createChannel = async (data) => await axiosInstead.post('/users/create-channel', data);
