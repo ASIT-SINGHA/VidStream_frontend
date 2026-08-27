c700ca6740f0279c9ee53c7db107cc839a9fe396
asit singha <asitkumarsingha34@gmail.com>
2026-08-27 12:33:08 +0530

    add auth store for user authentication management

---
+++ src/store/useAuthStore.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import {persist, createJSONStorage} from "zustand/middleware";
+
+const useAuthStore=create(persist((set)=>({
+    user:null,
+    accessToken:null,
+    isAuth:null,
+
+    login:(user,accessToken)=>(set({user,accessToken,isAuth:true})),
+    logout:()=>(set({user:null,accessToken:null,isAuth:null}))
+})
+),
+{
+    name:'vidstream-auth',
+    Storage:createJSONStorage(()=>sessionStorage),
+    partialize:(state)=>({
+        user:state.user,
+        accessToken:state.accessToken,
+        isAuth:state.isAuth,
+    })
+})
