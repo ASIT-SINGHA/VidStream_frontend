e45574b178cc6ebacff67123a8f1033df2f5ca48
asit singha <asitkumarsingha34@gmail.com>
2026-08-28 23:28:20 +0530

    fix: enhance validation messages for avatar and cover image in signUpSchema

--- src/utils/validateSchema.js
+++ src/utils/validateSchema.js
@@ -7,15 +7,21 @@ export const signUpSchema = z.object({
   email: z.string().email('Invalid email address'),
   password: z.string().min(6, 'password must be atleast 6 character'),
   avatar: z
-    .instanceof(FileList)
+    .instanceof(FileList, { message: 'Please upload a file' })
     .refine((files) => files?.length > 0, 'Avatar is required')
     .refine((files) => files[0]?.size <= 5000000, 'avatar must be less then 5 MB')
     .refine((files) => files[0]?.type.startsWith('image/'), 'Avatar must be a image'),
 
   coverImage: z
-    .instanceof(FileList)
-    .refine((files) => files[0]?.size <= 5000000, 'cover image must be less then 5 MB')
-    .refine((files) => files[0]?.type.startsWith('image/'), 'cover image must be a image'),
+    .instanceof(FileList, { message: 'Please upload a file' })
+    .refine(
+      (files) => files.length === 0 || files[0]?.size <= 5000000,
+      'cover image must be less then 5 MB',
+    )
+    .refine(
+      (files) => files.length === 0 || files[0]?.type.startsWith('image/'),
+      'cover image must be a image',
+    ),
 });
 
 export const loginSchema = z.object({
