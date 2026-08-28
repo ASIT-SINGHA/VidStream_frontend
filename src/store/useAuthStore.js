d4b755311fc70b4e85925ef8c9d56277110a00f5
asit singha <asitkumarsingha34@gmail.com>
2026-08-29 03:01:08 +0530

    fix: correct formatting and indentation in useAuthStore for improved readability

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -2,22 +2,24 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
 const useAuthStore = create(
-  persist((set) => ({
-    user: null,
-    accessToken: null,
-    isAuth: false,
+  persist(
+    (set) => ({
+      user: null,
+      accessToken: null,
+      isAuth: false,
 
-    login: (user, accessToken) => set({ user, accessToken, isAuth: true }),
-    logout: () => set({ user: null, accessToken: null, isAuth: false }),
-  })),
-  {
-    name: 'vidstream-auth',
-    Storage: createJSONStorage(() => sessionStorage),
-    partialize: (state) => ({
-      user: state.user,
-      accessToken: state.accessToken,
-      isAuth: state.isAuth,
+      login: (user, accessToken) => set({ user, accessToken, isAuth: true }),
+      logout: () => set({ user: null, accessToken: null, isAuth: false }),
     }),
-  },
+    {
+      name: 'vidstream-auth',
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({
+        user: state.user,
+        accessToken: state.accessToken,
+        isAuth: state.isAuth,
+      }),
+    },
+  ),
 );
 export default useAuthStore;
